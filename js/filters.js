35bbe38d1efb85e9818c3e9dce9df30c1c1204c0
Aleksey <hl-hanter1997@yandex.ru>
2019-07-19 02:06:43 +0500

    Обновление

--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,65 @@
 'use strict';
 
 (function () {
+  var asd = document.querySelector('.pictures');
+
+  function deletePictures() {
+    var childs = document.querySelectorAll('.picture');
+    for (var i = 0; i < childs.length; i++) {
+      childs[i].parentNode.removeChild(childs[i]);
+    }
+  }
+
+  function filterPop(data) {
+    deletePictures();
+    data.forEach(function (element) {
+      asd.appendChild(window.gallery.getDomElements(element));
+    });
+  }
+
+  function filterNew(data) {
+    deletePictures();
+    var newData = data.slice().sort(function () {
+      return Math.random() - 0.5;
+    });
+    for (var i = 0; i < window.constants.PHOTOS_QUANTITY; i++) {
+      var el = newData[i];
+      asd.appendChild(window.gallery.getDomElements(el));
+    }
+  }
+
+  function filterDisc(data) {
+    deletePictures();
+    var discussData = window.filters.sort(data);
+
+    discussData.forEach(function (element) {
+      asd.appendChild(window.gallery.getDomElements(element));
+    });
+  }
+
   window.filters = {
     unlock: function () {
       var filterPhoto = document.querySelector('.img-filters');
       if (filterPhoto.classList.contains('img-filters--inactive')) {
         filterPhoto.classList.remove('img-filters--inactive');
+      }
+    },
 
-        filterPhoto.querySelector('#filter-popular').addEventListener('click', window.filters.deletePictures);
+    set: function (evt, data) {
+      var buttonHoverClassName = 'img-filters__button--active';
+      document.querySelector('.' + buttonHoverClassName).classList.remove(buttonHoverClassName);
+      document.querySelector('#' + evt.target.id).classList.add(buttonHoverClassName);
+
+      switch (evt.target.id) {
+        case 'filter-popular':
+          window.utils.debounce(filterPop(data));
+          break;
+        case 'filter-new':
+          window.utils.debounce(filterNew(data));
+          break;
+        case 'filter-discussed':
+          window.utils.debounce(filterDisc(data));
+          break;
       }
     },
 
@@ -17,12 +69,5 @@
       });
       return popularPhotos;
     },
-
-    deletePictures: function () {
-      var childs = document.querySelectorAll('.picture');
-      for (var i = 0; i < childs.length; i++) {
-        childs[i].parentNode.removeChild(childs[i]);
-      }
-    }
   };
 })();
--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 'use strict';
 
 (function () {
-
+  // Переменные для работы с просмотром загруженной картинки
   var uploadPhoto = document.querySelector('#upload-file');
   var uploadPhotoSetting = document.querySelector('.img-upload__overlay');
   var uploadCancel = document.querySelector('#upload-cancel');
   var uploadPreview = document.querySelector('.img-upload__preview');
+  var uploadHastTags = uploadPhotoSetting.querySelector('.text__hashtags');
 
-  // // Переменные для работы с фильтрами
+  // Переменные для работы с фильтрами
   var uploadPreviewPhoto = uploadPreview.querySelector('img');
   var listItem = document.querySelector('.effects__list');
   var uploadFileEffectNone = listItem.querySelector('#effect-none');
   var uploadFilterSlider = document.querySelector('.img-upload__effect-level');
 
+  // Функция для работы с кнопками фильтрации изображения
   window.data.load(function (data) {
     data.forEach(function (element) {
       document.querySelector('.pictures').appendChild(window.gallery.getDomElements(element));
@@ -26,65 +28,25 @@
 
     // Переменные для работы с кнопками переключения окон
     var filterPhoto = document.querySelector('.img-filters');
-    var popularPhoto = filterPhoto.querySelector('#filter-popular');
-    var newPhoto = filterPhoto.querySelector('#filter-new');
-    var discussedPhoto = filterPhoto.querySelector('#filter-discussed');
+    var filterPhotos = filterPhoto.querySelector('.img-filters__form');
 
-    function setFilter(evt) {
-
-      var buttonHover = 'img-filters__button--active';
-      document.querySelector('.' + buttonHover).classList.remove(buttonHover);
-      document.querySelector('#' + evt.currentTarget.id).classList.add(buttonHover);
-      // TODO: добить задачу с исключением дребезга!
-      function filterPop() {
-        window.filters.deletePictures();
-        data.forEach(function (element) {
-          document.querySelector('.pictures').appendChild(window.gallery.getDomElements(element));
-        });
-      }
-
-      function filterNew() {
-        window.filters.deletePictures();
-        var newData = data.slice().sort(function () {
-          return Math.random() - 0.5;
-        });
-        for (var i = 0; i < window.constants.PHOTOS_QUANTITY; i++) {
-          var el = newData[i];
-          document.querySelector('.pictures').appendChild(window.gallery.getDomElements(el));
-        }
-      }
-
-      function filterDisc() {
-        window.filters.deletePictures();
-        var discussData = window.filters.sort(data);
-
-        discussData.forEach(function (element) {
-          document.querySelector('.pictures').appendChild(window.gallery.getDomElements(element));
-        });
-      }
-
-      switch (evt.currentTarget.id) {
-        case 'filter-popular':
-          window.utils.debounce(filterPop);
-          break;
-        case 'filter-new':
-          window.utils.debounce(filterNew);
-          break;
-        case 'filter-discussed':
-          window.utils.debounce(filterDisc);
-          break;
-      }
-    }
-
-    popularPhoto.addEventListener('click', setFilter);
-    newPhoto.addEventListener('click', setFilter);
-    discussedPhoto.addEventListener('click', setFilter);
+    filterPhotos.addEventListener('click', function (evt) {
+      window.filters.set(evt, data);
+    });
   });
 
   function onChangeEffect(evt) {
     var target = evt.target;
     if (target.name === 'effect') {
       window.form.selectFilter(target.id);
+    }
+  }
+
+  function onPopupEscPress(evt) {
+    if (evt.keyCode === window.constants.ESC_KEY
+      && !evt.currentTarget.querySelector('.text__description:focus')
+      && !evt.currentTarget.querySelector('.text__hashtags')) {
+      closePopup();
     }
   }
 
@@ -98,14 +60,45 @@
 
     uploadFilterSlider.classList.add('hidden');
     listItem.addEventListener('click', onChangeEffect);
-  }
 
-  function onPopupEscPress(evt) {
-    if (evt.keyCode === window.constants.ESC_KEY
-      && !evt.currentTarget.querySelector('.text__description:focus')
-      && !evt.currentTarget.querySelector('.text__hashtags')) {
-      closePopup();
-    }
+    // Вешаем обработчик на строку с хеш-тегами
+    uploadHastTags.addEventListener('change', function (evt) {
+      var currentValue = evt.target.value;
+      if (currentValue) {
+        var checkedTags = [];
+        var hashTags = [];
+        hashTags = currentValue.split(' ');
+        // console.log(hashTags);
+        var element = '#';
+        // Проверяем длинну "слова" и наличие "#"
+        for (var i = 0; i < hashTags.length; i++) {
+          if (hashTags[i].indexOf(element) === 0) {
+            // console.log(hashTags[i].length);
+            // console.log(hashTags[i].toLowerCase());
+
+            // Делаем проверку на существование похожего хеш-тега
+            if (i === 0) {
+              // console.log(3 + ' serv');
+              checkedTags.push(hashTags[i]);
+            } else {
+              // console.log(checkedTags.indexOf(hashTags[i]));
+              // console.log(checkedTags.indexOf(hashTags[i]) === -1);
+              if (checkedTags.indexOf(hashTags[i]) === -1) {
+                // console.log(4 + ' serv');
+                checkedTags.push(hashTags[i]);
+              } else {
+                // console.log(5 + ' Есть похожий элемент');
+                // Сообщение об ошибке
+              }
+            }
+          } else {
+            // console.log(0 + ' # не стоит на 1 месте');
+          }
+        }
+
+      }
+
+    });
   }
 
   function closePopup() {
@@ -121,8 +114,6 @@
     uploadScaleIncrease.removeEventListener('click', window.form.onIncreasePhoto);
     listItem.removeEventListener('click', onChangeEffect);
   }
-
   uploadPhoto.addEventListener('change', openPopup);
-
   uploadCancel.addEventListener('click', closePopup);
 })();
